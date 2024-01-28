2cf8caaaf7d67b58d6fb3e4c2091db6a043a814f
jsayev <cisayev@gmail.com>
2024-01-28 17:25:36 +0100

    Renamed package and added countPartitions

--- src/examples/java/RecursiveExamples.java
+++ src/examples/java/RecursiveExamples.java
@@ -1,8 +1,8 @@
-package java.examples;
+package examples.java;
 
 class RecursiveExamples {
     public static void main(String[] args) {
-
+        System.out.println(countPartitions(6, 4));
     }
 
     /**
@@ -18,8 +18,7 @@ class RecursiveExamples {
     }
 
     /**
-     *
-     * @param row horizontal cells count
+     * @param row    horizontal cells count
      * @param column vertical cells count
      * @return count of unique paths possible from (0,0) cell to (row,column) cell
      */
@@ -34,7 +33,18 @@ class RecursiveExamples {
         return uniquePathsCount(row - 1, column) + uniquePathsCount(row, column - 1);
     }
 
+    /**
+     * @param n number of objects/items
+     * @param m max number of unique partitioning allowed
+     * @return total number of unique partitioning
+     */
     static int countPartitions(int n, int m) {
-        return 4;
+        if (n == 0) {
+            return 1;
+        }
+        if (m == 0 || n < 0) {
+            return 0;
+        }
+        return countPartitions(n - m, m) + countPartitions(n, m - 1);
     }
 }
